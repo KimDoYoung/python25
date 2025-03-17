b3576f2b23a7708acdfb53b24534e83e51d1b1b9
KimDoYoung <kdy987@gmail.com>
2025-03-17 13:54:36 +0900

    0317

--- kindscrap2025/make.sh
+++ kindscrap2025/make.sh
@@ -15,6 +15,16 @@ pyinstaller --onefile --name="$EXE_NAME" main.py
 # 5. $HOME/bin으로 실행 파일 복사
 mkdir -p "$HOME/bin"
 cp "dist/$EXE_NAME" "$HOME/bin/"
+rm -rf "/c/kindscrap/"
+mkdir -p "/c/kindscrap/"
+cp "dist/$EXE_NAME" "/c/kindscrap/"
+cp ".env.real" "/c/kindscrap/.env.real"
+cp "run_kindscrap.bat" "/c/kindscrap/run_kindscrap.bat"
 
 # 6. 완료 메시지 출력
 echo "✅ 빌드 완료: $HOME/bin/$EXE_NAME"
+
+echo "✅ copy 완료: /c/kindscrap/$EXE_NAME"
+echo "✅ copy 완료: /c/kindscrap/.env.real"
+echo "✅ copy 완료: /c/kindscrap/run_kindscrap.bat"
+
