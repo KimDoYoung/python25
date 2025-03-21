ec820efbc0f0c7c67e4dbe0a89a4f0f84b894c3c
KimDoYoung <kdy987@gmail.com>
2025-03-21 16:50:39 +0900

    0321

--- rpa/kavana/lib/core/commands/rpa/key_in_command.py
+++ rpa/kavana/lib/core/commands/rpa/key_in_command.py
@@ -31,3 +31,27 @@ class KeyInCommand(BaseCommand):
         else:
             raise KavanaSyntaxError("KEY_IN 명령어는 인자로 문자열 또는 리스트가 필요합니다.")
 
+        # 속도(speed) 옵션 파싱
+        speed = self.extract_option_value(args[i:], executor, option_map, option_values)["speed"]
+
+        # keys_to_press = []
+
+        # if first_token.type == TokenType.STRING:
+        #     keys_to_press.append(first_token.value)
+
+        # elif first_token.type == TokenType.LIST_EX:
+        #     for item in first_token.value:
+        #         if item.type != TokenType.STRING:
+        #             raise KavanaSyntaxError("KEY_IN 리스트 내부는 모두 문자열이어야 합니다.")
+        #         keys_to_press.append(item.value)
+
+        # # 키 입력 실행
+        # for key_str in keys_to_press:
+        #     key_str = key_str.strip().lower()
+        #     if '+' in key_str:
+        #         key_parts = [k.strip() for k in key_str.split('+')]
+        #         rpa_manager.key_hotkey(*key_parts)
+        #     else:
+        #         rpa_manager.key_press(key_str)
+        #     if speed > 0:
+        #         rpa_manager.sleep(speed)
