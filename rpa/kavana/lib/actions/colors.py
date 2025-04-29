372b5edf9944426aa4ac73436201fc168a50718a
KimDoYoung <kdy987@gmail.com>
2025-04-29 23:57:07 +0900

    0429

--- rpa/kavana/lib/actions/colors.py
+++
@@ -1,9 +0,0 @@
-def is_similar_color(color1, color2, tolerance=10):
-    """
-    두 개의 RGB 색상이 비슷한지 확인 (tolerance 값 내에서 허용)
-    :param color1: 기준 색상 (예: (34, 177, 76))
-    :param color2: 비교할 색상 (예: (36, 180, 78))
-    :param tolerance: 허용할 오차 범위 (기본값 10)
-    :return: 비슷하면 True, 아니면 False
-    """
-    return all(abs(c1 - c2) <= tolerance for c1, c2 in zip(color1, color2))
--- rpa/kavana/lib/actions/scales.py
+++
@@ -1,5 +0,0 @@
-import ctypes
-
-def get_scale_factor() -> float:
-    """ 현재 사용자의 화면 DPI 스케일 팩터를 반환합니다. """
-    return ctypes.windll.shcore.GetScaleFactorForDevice(0) / 100
