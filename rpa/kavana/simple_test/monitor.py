5dd14f1d86fcc8bc97dd743ecdc9c595a040e33b
KimDoYoung <kdy987@gmail.com>
2025-05-09 13:32:13 +0900

    기능 개선: 모니터 정보 출력에 기본 모니터 표시 추가

--- rpa/kavana/simple_test/monitor.py
+++ rpa/kavana/simple_test/monitor.py
@@ -3,4 +3,7 @@ from screeninfo import get_monitors
 monitors = get_monitors()
 
 for i, m in enumerate(monitors):
-    print(f"[{i}] {m.name} - {m.width}x{m.height} at ({m.x}, {m.y})")
+    p = ""
+    if m.is_primary:
+        p = "primary"
+    print(f"[{i}] {m.name} - {m.width}x{m.height} at ({m.x}, {m.y}) {p}")
