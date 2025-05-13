12e40b298e3b9255dad82798fdec4168df994bad
KimDoYoung <kdy987@gmail.com>
2025-05-13 12:08:04 +0900

    기능: README.md 업데이트 및 main.py에 OCR 처리 로직 추가, pyproject.toml 및 uv.lock에 의존성 추가

--- ai_ocr/tesseract-0/README.md
+++ ai_ocr/tesseract-0/README.md
@@ -3,3 +3,9 @@
 ## 설명
 
 1. tesseract 테스트
+
+## 설치
+1. tesseract 설치
+2. uv add pytesseract pillow
+3. 사진/OCR_TEST
+   
--- ai_ocr/tesseract-0/main.py
+++ ai_ocr/tesseract-0/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from tesseract-0!")
+import os
 
+import pytesseract
+from PIL import Image
+
+def main():
+    base_dir = os.path.expanduser("~/Pictures/OCR_TEST")
+    print(f"Base directory is set to: {base_dir}")
+
+    # config = '--oem 3 --psm 3 -l kor+eng'
+    # config = '--oem 3 --psm 3 -l kor'
+    config = '--oem 1 --psm 3 -l kor'
+    for no in range(0, 7):
+        image_path = os.path.join(base_dir, f"image_{no}.png")
+        if not os.path.exists(image_path):
+            print(f"File {image_path} does not exist.")
+            exit(1)
+        with Image.open(image_path) as img:
+            width, height = img.size
+            resized_img = img.resize((int(width * 1.5), int(height * 1.5)))
+            temp_image_path = os.path.join(base_dir, f"temp_image_{no}.png")
+            resized_img.save(temp_image_path)
+            image_path = temp_image_path
+        text = pytesseract.image_to_string(image_path, config=config)
+        print("-" * 30)
+        print(f"{image_path} OCR result:")
+        print(text)
+        print("-" * 30)
 
 if __name__ == "__main__":
     main()
